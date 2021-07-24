7d34769cf4bd60862e499fb3e31c6688559d9cdf
Gabriel DORIATH DOHLER <gabriel.doriath.dohler@ens.psl.eu>
2021-07-24 23:37:55 +0200

    Add mkReverseRecord

--- dns/util/default.nix
+++ dns/util/default.nix
@@ -23,6 +23,30 @@ let
     then ''"${s}"''
     else concatMapStringsSep " " (x: ''"${x}"'') (splitInGroupsOf 255 s);
 
+  # : str -> str, with length 4 (zeros are padded to the left)
+  align4Bytes = lib.fixedWidthString 4 "0";
+
+  # : int -> str -> str
+  # Expands "" to 4n zeros and aligns the rest on 4 bytes
+  align4BytesOrExpand = n: v:
+    if v == ""
+    then (lib.fixedWidthString (4*n) "0" "")
+    else align4Bytes v;
+
+  # : str -> [ str ]
+  # Returns the record of the ipv6 as a list
+  mkRecordAux = v6:
+    let
+      splitted = lib.splitString ":" v6;
+      n = 8 - builtins.length (lib.filter (x: x != "") splitted);
+    in
+      lib.stringToCharacters (lib.concatMapStrings (align4BytesOrExpand n) splitted);
+
+  # : str -> str
+  # Returns the reversed record of the ipv6
+  mkReverseRecord = v6:
+    lib.concatStringsSep "." (lib.reverseList (mkRecordAux v6)) + ".ip6.arpa";
+
 in {
-  inherit writeCharacterString;
+  inherit writeCharacterString mkReverseRecord;
 }
