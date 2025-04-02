46d8f8698a9d22775cbdcf38893d087046f8d36f
Sophie Taylor <sophie@spacekitteh.moe>
2025-04-02 10:37:25 +1000

    HTTPS: Fix case nameFixup for when port is null

--- dns/types/records/HTTPS.nix
+++ dns/types/records/HTTPS.nix
@@ -1,4 +1,4 @@
 args@{lib, ...}: import ./SVCB.nix args // {
   rtype = "HTTPS";
-  nameFixup = name: self: if self.port == 443 then name else "_${self.port}._https.${name}";
+  nameFixup = name: self: if self.port == null || self.port == 443 then name else "_${self.port}._https.${name}";
 }
